9911b89fe28c09f8f5d0484ccd2dba96e3db46ba
marwinburesch <marwin@posteo.de>
2020-03-24 09:21:18 +0100

    Restructure files, add modules

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const { get, set, unset } = require('./lib/commands')
-const [command, key, value] = process.argv.slice(2)
+const { get, set, unset } = require('./lib/queries/api');
+const [command, key, value] = process.argv.slice(2);
 
-
-
-if (command === "get") {
-  get(key)
-} else if (command === "set") {
-  set(key, value)
-} else if (command === "unset") {
-  unset(key)
+if (command === 'get') {
+    get(key);
+} else if (command === 'set') {
+    set(key, value);
+} else if (command === 'unset') {
+    unset(key);
 } else {
-  console.log("this command is unknown")
+    console.log('this command is unknown');
 }
